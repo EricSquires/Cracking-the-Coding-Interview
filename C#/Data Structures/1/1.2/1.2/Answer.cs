0d9ce8d128c258161fa53af429c70606841785a6
Eric Squires <eric.h.squires@gmail.com>
2015-07-25 13:28:03 -0400

    Committing initial solution

---
+++ C#/Data Structures/1/1.2/1.2/Answer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2
+{
+    public static class Answer
+    {
+        // Naive solution
+
+        /// <summary>
+        /// Returns true if this string (<paramref name="s1"/>) is a permutation of the argument string (<paramref name="s2"/>)
+        /// </summary>
+        public static bool IsPermutation(this string s1, string s2)
+        {
+            if(s1 == s2)
+            {
+                return true;
+            }
+
+            // If the strings aren't of equal length, one cannot be a permutation of the other.
+            if(s2.Length != s1.Length)
+            {
+                return false;
+            }
+
+            for(var i = 1; i < s2.Length; i++)
+            {
+                if(s1 == s2.Substring(i, s2.Length - i) + s2.Substring(0, i))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C#/Data Structures/1/1.2/Tests/AnswerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1._2;
+
+namespace Tests
+{
+    [TestClass]
+    public class AnswerTests
+    {
+        [TestMethod]
+        public void TrueTest()
+        {
+            Assert.IsTrue("abcde".IsPermutation(""));
+        }
+    }
+}
