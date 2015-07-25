9e11366a9add80d18813a9bcec3a9737f4839458
Eric Squires <eric.h.squires@gmail.com>
2015-07-25 12:35:23 -0400

    Committing initial solution

---
+++ C#/Data Structures/1/1.1/1.1/Answer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    public static class Answer
+    {
+        // Naive solution
+        public static bool IsUnique(this string txt)
+        {
+            bool ret = true;
+
+            for(var i = 0; ret && i < txt.Length; i++)
+            {
+                for(var j = i; ret && j < txt.Length; j++)
+                {
+                    ret = txt[0] != txt[1];
+                }
+            }
+
+            return ret;
+        }
+    }
+}
