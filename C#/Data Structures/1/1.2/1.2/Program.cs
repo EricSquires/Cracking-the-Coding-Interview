36b8fb47a667929ca88630c5668c7b91457b979c
Eric Squires <eric.h.squires@gmail.com>
2015-07-25 13:43:25 -0400

    Added simple CLI for manual testing

--- C#/Data Structures/1/1.2/1.2/Program.cs
+++ C#/Data Structures/1/1.2/1.2/Program.cs
@@ -10,6 +10,26 @@ namespace _1._2
     {
         static void Main(string[] args)
         {
+            string input1, input2;
+
+            do
+            {
+                Console.Write("Enter string 1: ");
+                input1 = Console.ReadLine().Trim();
+
+                Console.Write("Enter string 2: ");
+                input2 = Console.ReadLine().Trim();
+
+                if(string.IsNullOrWhiteSpace(input1) && string.IsNullOrWhiteSpace(input2))
+                {
+                    Console.WriteLine("Exiting...");
+                }
+                else
+                {
+                    Console.WriteLine($"\"{input1}\".IsPermutation(\"{input2}\"): {input1.IsPermutation(input2)}");
+                }
+            }
+            while(!string.IsNullOrWhiteSpace(input1) || !string.IsNullOrWhiteSpace(input2));
         }
     }
 }
