3ed14d5101fc50302629449bb9fd2ef3b22f66a8
Eric Squires <eric.h.squires@gmail.com>
2015-07-25 13:33:14 -0400

    Adding basic tests

--- C#/Data Structures/1/1.2/Tests/AnswerTests.cs
+++ C#/Data Structures/1/1.2/Tests/AnswerTests.cs
@@ -10,7 +10,29 @@ namespace Tests
         [TestMethod]
         public void TrueTest()
         {
-            Assert.IsTrue("abcde".IsPermutation(""));
+            Assert.IsTrue(string.Empty.IsPermutation(string.Empty));
+
+            Assert.IsTrue("a".IsPermutation("a"));
+
+            Assert.IsTrue("ab".IsPermutation("ab"));
+            Assert.IsTrue("ab".IsPermutation("ba"));
+
+            Assert.IsTrue("abcde".IsPermutation("abcde"));
+            Assert.IsTrue("abcde".IsPermutation("eabcd"));
+            Assert.IsTrue("abcde".IsPermutation("deabc"));
+            Assert.IsTrue("abcde".IsPermutation("cdeab"));
+            Assert.IsTrue("abcde".IsPermutation("bcdea"));
+        }
+
+        [TestMethod]
+        public void FalseTest()
+        {
+            Assert.IsFalse("a".IsPermutation(null));
+            Assert.IsFalse("a".IsPermutation(string.Empty));
+            Assert.IsFalse("a".IsPermutation("b"));
+
+            Assert.IsFalse("abc".IsPermutation("bac"));
+            Assert.IsFalse("abc".IsPermutation("abcd"));
         }
     }
 }
