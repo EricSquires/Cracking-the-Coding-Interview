b2265120477e417adf7375f220f5ff5e94b2e113
Eric Squires <eric.h.squires@gmail.com>
2015-07-25 08:18:51 -0400

    Initial commit for unit tests. Includes some tests for ctors.

---
+++ C#/Data Structures/1/Hash Tables/Tests/HashTableTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HashTables;
+
+namespace Tests
+{
+    [TestClass]
+    public class HashTableTests
+    {
+        private static readonly Func<string, int> _SIMPLE_STRING_HASH = (string item) => item[0];
+        #region Ctors
+
+        [TestMethod]
+        public void StringConstructors()
+        {
+            new HashTable<string>();
+            new HashTable<string>(_SIMPLE_STRING_HASH);
+            new HashTable<string>(_SIMPLE_STRING_HASH, 10);
+        }
+
+        [TestMethod]
+        public void IntConstructors()
+        {
+            new HashTable<int>();
+            new HashTable<int>(10);
+        }
+
+        #endregion
+    }
+}
