8be6fa6004e3af866174f379e2704dc675aed196
Eric Squires <eric.h.squires@gmail.com>
2015-07-25 14:56:34 -0400

    Added legitimate solution to problem in AnswerSelfImplemented. Added description of URLify method in IAnswer.

---
+++ C#/Data Structures/1/1.3/1.3/AnswerSelfImplemented.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3
+{
+    public class AnswerSelfImplemented : IAnswer
+    {
+        public string URLify(string txt)
+        {
+            for(var i = txt.Length - 1; i >= 0; i--)
+            {
+                if(txt[i] == ' ')
+                {
+                    txt.Remove(i, 1);
+                    txt.Insert(i, "%20");
+                }
+            }
+
+            return txt;
+        }
+    }
+}
--- C#/Data Structures/1/1.3/1.3/IAnswer.cs
+++ C#/Data Structures/1/1.3/1.3/IAnswer.cs
@@ -8,6 +8,9 @@ namespace _1._3
 {
     public interface IAnswer
     {
+        /// <summary>
+        /// Despite its name, this function only replaces spaces with %20.
+        /// </summary>
         string URLify(string txt);
     }
 }
