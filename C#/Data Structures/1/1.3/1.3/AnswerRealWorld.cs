b39360c7166795d1ce5368da9d529e06e69dc2ad
Eric Squires <eric.h.squires@gmail.com>
2015-07-25 14:48:06 -0400

    Initial checkin of simple solution.

---
+++ C#/Data Structures/1/1.3/1.3/AnswerRealWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3
+{
+    public class AnswerRealWorld : IAnswer
+    {
+        public string URLify(string txt)
+        {
+            return txt.Replace(" ", "%20");
+        }
+    }
+}
