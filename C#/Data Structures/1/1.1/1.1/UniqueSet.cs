32895bba2347c853b4b7bc6ae75b4cda7da7b37d
Eric Squires <eric.h.squires@gmail.com>
2015-07-27 21:34:25 -0400

    Adding a smarter solution that utilizes HashSet along with unit tests

---
+++ C#/Data Structures/1/1.1/1.1/UniqueSet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    public class UniqueSet : IUniqueTester
+    {
+        // O(n) thanks to O(1) insertion into the set
+        public bool IsUnique(string txt)
+        {
+            if(txt == null)
+            {
+                return true;
+            }
+
+            var set = new HashSet<char>();
+
+            foreach(var c in txt)
+            {
+                if(!set.Add(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
