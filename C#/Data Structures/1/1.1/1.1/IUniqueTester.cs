9635bb95e6b986020baa42023d33b28529f5b6f9
Eric Squires <eric.h.squires@gmail.com>
2015-07-27 21:24:22 -0400

    Apparently Visual Studio didn't feel like automatically adding the new files.

---
+++ C#/Data Structures/1/1.1/1.1/IUniqueTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    public interface IUniqueTester
+    {
+        /// <summary>
+        /// Returns true if there are no repeated characters in the string.
+        /// </summary>
+        bool IsUnique(string txt);
+    }
+}
---
+++ C#/Data Structures/1/1.1/Tests/UniqueNaiveTests.cs
@@ -0,0 +1,10 @@
+﻿using _1._1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UniqueNaiveTests : TestBase<UniqueNaive>
+    {
+    }
+}
