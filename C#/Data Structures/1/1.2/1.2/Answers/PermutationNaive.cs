622e390f114209acabae07c2442742e34718c634
Eric Squires <eric.h.squires@gmail.com>
2015-07-30 22:50:48 -0400

    Move PermutationNaive to proper namespace

--- C#/Data Structures/1/1.2/1.2/Answers/PermutationNaive.cs
+++ C#/Data Structures/1/1.2/1.2/Answers/PermutationNaive.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1._2
+namespace _1._2.Answers
 {
     public class PermutationNaive : IPermutationChecker
     {
--- C#/Data Structures/1/1.2/Tests/NaiveTests.cs
+++ C#/Data Structures/1/1.2/Tests/NaiveTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using _1._2;
+using _1._2.Answers;
 
 namespace Tests
 {
