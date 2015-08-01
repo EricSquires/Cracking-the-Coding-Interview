f729df8b6a963149b954018859402d2b1fe4b540
Eric Squires <eric.h.squires@gmail.com>
2015-08-01 03:09:08 -0400

    Implement PerformanceProgramBase

--- C#/Data Structures/1/1.4/1.4/Program.cs
+++ C#/Data Structures/1/1.4/1.4/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _1._4.Answers;
+using PerformanceTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,86 @@ using System.Threading.Tasks;
 
 namespace _1._4
 {
-    class Program
+    class Program : PerformanceProgramBase
     {
+        private readonly Random _rand = new Random();
+
         static void Main(string[] args)
         {
+            var obj = new Program();
+            obj.Run(args);
+        }
+
+        protected override void InteractiveMode(string[] args)
+        {
+            IPalindromePermutationChecker checker = new PalPermNaive();
+
+            string input;
+
+            do
+            {
+                Console.Write("Enter string: ");
+                input = Console.ReadLine();
+
+                if(!string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine($"'{input}': {checker.IsPalindromePermutation(input)}");
+                }
+            }
+            while(string.IsNullOrWhiteSpace(input));
+        }
+
+        protected override void TimingMode(string[] args)
+        {
+            IPalindromePermutationChecker[] testObjs = new[]
+                {
+                    new PalPermNaive()
+                };
+
+            foreach(var testObj in testObjs)
+            {
+                double avgTime = Time(testObj.IsPalindromePermutation, (int _) => GetTestString(true));
+                Console.WriteLine($"{testObj.GetType().ToString(),40} permutation    : {avgTime:N5} ms");
+
+                avgTime = Time(testObj.IsPalindromePermutation, (int _) => GetTestString(false));
+                Console.WriteLine($"{testObj.GetType().ToString(),40} non-permutation: {avgTime:N5} ms");
+            }
+        }
+
+        private string GetTestString(bool isPalindrome)
+        {
+            const int _MIN_LENGTH = 20;
+            const int _MAX_LENGTH = 50;
+
+            const int _CHAR_MIN = 'A';
+            const int _CHAR_MAX = 'z';
+
+            string ret = string.Empty;
+
+            var len = _rand.Next(_MIN_LENGTH, _MAX_LENGTH);
+
+            if(isPalindrome)
+            {
+                len = len/2;
+            }
+
+            for(var i = 0; i < len; i++)
+            {
+                ret += (char)_rand.Next(_CHAR_MIN, _CHAR_MAX);
+            }
+            
+            if(isPalindrome)
+            {
+                string secondHalf = ret;
+                if(len % 2 == 1)
+                {
+                    ret += (char)_rand.Next(_CHAR_MIN, _CHAR_MAX);
+                }
+
+                ret += secondHalf;
+            }
+
+            return ret;
         }
     }
 }
