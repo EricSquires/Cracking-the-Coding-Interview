92794f7ef4165317d7f0c596c18061b2ebef1177
Eric Squires <eric.h.squires@gmail.com>
2015-08-01 16:33:48 -0400

    Add and implement PerformanceTesting library

--- C#/Data Structures/1/1.3/1.3/Program.cs
+++ C#/Data Structures/1/1.3/1.3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PerformanceTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,20 @@ using System.Threading.Tasks;
 
 namespace _1._3
 {
-    class Program
+    class Program : PerformanceProgramBase
     {
+        private const int _MIN_TEST_STR_LEN = 10;
+        private const int _MAX_TEST_STR_LEN = 30;
+
+        private readonly Random _rand = new Random();
+
         static void Main(string[] args)
+        {
+            var obj = new Program();
+            obj.Run(args);
+        }
+
+        protected override void InteractiveMode(string[] args)
         {
             IAnswer answer = new AnswerSelfImplemented();
 
@@ -29,6 +41,55 @@ namespace _1._3
                 }
             }
             while (!string.IsNullOrWhiteSpace(input));
+        }
+
+        protected override void TimingMode(string[] args)
+        {
+            var testObjs = new IAnswer[]
+                {
+                    new AnswerRealWorld(),
+                    new AnswerSelfImplemented()
+                };
+
+            foreach(var testObj in testObjs)
+            {
+                double avgTime = Time(testObj.URLify, (int _) => GetTestString());
+                Console.WriteLine($"{testObj.GetType().ToString(),40} : {avgTime:N5} ms");
+            }
+
+            Console.ReadKey();
+        }
+
+        private string GetTestString()
+        {
+            const int _MIN_CHAR = 'a';
+            const int _MAX_CHAR = 'z';
+
+            var ret = new StringBuilder();
+
+            int len = _rand.Next(_MIN_TEST_STR_LEN, _MAX_TEST_STR_LEN);
+            for(var i = 0; i < len; i++)
+            {
+                ret.Append((char)_rand.Next(_MIN_CHAR, _MAX_CHAR));
+            }
+
+            int numSpaces = _rand.Next(5);
+            var spaces = new bool[ret.Length];
+            for(var i = 0; i < numSpaces; i++)
+            {
+                int idx;
+                
+                do
+                {
+                    idx = _rand.Next(ret.Length);
+                }
+                while(spaces[idx]);
+
+                spaces[idx] = true;
+                ret[idx] = ' ';
+            }
+
+            return ret.ToString();
         }
     }
 }
