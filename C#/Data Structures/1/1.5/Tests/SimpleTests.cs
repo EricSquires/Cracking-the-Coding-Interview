4461a7c2084be5b5e861058189af2aeb01d8f88f
Eric Squires <eric.h.squires@gmail.com>
2015-08-02 15:44:23 -0400

    Add tests

---
+++ C#/Data Structures/1/1.5/Tests/SimpleTests.cs
@@ -0,0 +1,15 @@
+﻿using _1_5.Answers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class SimpleTests : TestBase<OneAwaySimple>
+    {
+    }
+}
---
+++ C#/Data Structures/1/1.5/Tests/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using _1_5.Answers;
+
+namespace Tests
+{
+    [TestClass]
+    public abstract class TestBase<T> where T : IOneAwayChecker, new()
+    {
+        protected T TestObject { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            TestObject = new T();
+        }
+
+        [TestMethod]
+        public void OneAwayTests()
+        {
+            Assert.IsTrue(TestObject.IsOneAway("pale", "ple"));
+            Assert.IsTrue(TestObject.IsOneAway("ple", "pale"));
+            Assert.IsTrue(TestObject.IsOneAway("pales", "pale"));
+            Assert.IsTrue(TestObject.IsOneAway("pale", "pales"));
+            Assert.IsTrue(TestObject.IsOneAway("pale", "bale"));
+            Assert.IsTrue(TestObject.IsOneAway("bale", "pale"));
+        }
+
+        [TestMethod]
+        public void MoreAwayTests()
+        {
+            Assert.IsFalse(TestObject.IsOneAway("pale", "bake"));
+            Assert.IsFalse(TestObject.IsOneAway("bake", "pale"));
+            Assert.IsFalse(TestObject.IsOneAway("pe", "pale"));
+            Assert.IsFalse(TestObject.IsOneAway("pale", "pe"));
+        }
+    }
+}
