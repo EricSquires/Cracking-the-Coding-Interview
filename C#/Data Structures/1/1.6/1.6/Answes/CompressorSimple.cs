210b539b252487b73f6f5415f8c9c43e3d767ec9
Eric Squires <eric.h.squires@gmail.com>
2015-08-02 16:03:18 -0400

    Add 1.6 initial solution

---
+++ C#/Data Structures/1/1.6/1.6/Answes/CompressorSimple.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._6.Answes
+{
+    public class CompressorSimple : ICompressor
+    {
+        public string Compress(string txt)
+        {
+            var ret = new StringBuilder();
+
+            int count = 0;
+            char currentChar = '\0';
+
+            for(var i = 0; i < txt.Length; i++, count++)
+            {
+                if(txt[i] != currentChar)
+                {
+                    if (i != 0)
+                    {
+                        ret.Append(currentChar);
+                        if (count > 1)
+                        {
+                            ret.Append(count);
+                        }
+                    }
+
+                    currentChar = txt[i];
+                    count = 0;
+                }
+            }
+
+            ret.Append(currentChar);
+            if (count > 1)
+            {
+                ret.Append(count);
+            }
+
+            return ret.ToString();
+        }
+    }
+}
---
+++ C#/Data Structures/1/1.6/1.6/Answes/ICompressor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._6.Answes
+{
+    public interface ICompressor
+    {
+        /// <summary>
+        /// Compresses the given string.
+        /// </summary>
+        /// <param name="txt">The string to compress.</param>
+        /// <returns>The string after compression.</returns>
+        string Compress(string txt);
+    }
+}
