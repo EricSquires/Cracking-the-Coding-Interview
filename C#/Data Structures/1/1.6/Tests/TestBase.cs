51156c568a5c11fee2d572c63828d974858f6051
Eric Squires <eric.h.squires@gmail.com>
2015-08-02 16:17:44 -0400

    Add tests

---
+++ C#/Data Structures/1/1.6/Tests/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1._6.Answes;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestBase<T> where T : ICompressor, new()
+    {
+        protected T TestObject { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            TestObject = new T();
+        }
+
+        [TestMethod]
+        public void Null()
+        {
+            Assert.AreEqual(TestObject.Compress(null), null);
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.AreEqual(TestObject.Compress(string.Empty), string.Empty);
+        }
+
+        [TestMethod]
+        public void NormalTests()
+        {
+            Assert.AreEqual(TestObject.Compress("abc"), "abc");
+            Assert.AreEqual(TestObject.Compress("aabbcc"), "a2b2c2");
+            Assert.AreEqual(TestObject.Compress("aabaabcaac"), "a2ba2bca2c");
+            Assert.AreEqual(TestObject.Compress("aabcccccaaa"), "a2bc5a3");
+        }
+    }
+}
---
+++ C#/Data Structures/1/1.6/Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1._6.Answes;
+
+namespace Tests
+{
+    [TestClass]
+    public class SimpleTests : TestBase<CompressorSimple>
+    {
+    }
+}
