817cea13226e9ca6fb32d148bb5e04a7a0d25ad2
Eric Squires <eric.h.squires@gmail.com>
2015-08-02 16:09:20 -0400

    Add PerformanceProgramBase implementation

--- C#/Data Structures/1/1.6/1.6/Program.cs
+++ C#/Data Structures/1/1.6/1.6/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _1._6.Answes;
+using PerformanceTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,38 @@ using System.Threading.Tasks;
 
 namespace _1._6
 {
-    class Program
+    class Program : PerformanceProgramBase
     {
         static void Main(string[] args)
         {
+            var obj = new Program();
+            obj.Run(new string[] { "-i" });
+        }
+
+        protected override void InteractiveMode(string[] args)
+        {
+            ICompressor checker = new CompressorSimple();
+
+            string input;
+
+            do
+            {
+                Console.Write("Enter string: ");
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine($"'{input}': '{checker.Compress(input)}'");
+                }
+            }
+            while (string.IsNullOrWhiteSpace(input));
+
+            Console.ReadKey();
+        }
+
+        protected override void TimingMode(string[] args)
+        {
+            throw new NotImplementedException();
         }
     }
 }
