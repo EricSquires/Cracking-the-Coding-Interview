97309ce6322dac492cef5a0af70fac8d21b02b2d
Eric Squires <eric.h.squires@gmail.com>
2015-08-17 22:54:01 -0400

    Add constructors for Matrix and Pixel to facilitate testing

--- C#/Data Structures/1/1.7/1.7/DataStructures/Matrix.cs
+++ C#/Data Structures/1/1.7/1.7/DataStructures/Matrix.cs
@@ -30,5 +30,26 @@ namespace _1._7.DataStructures
         {
             _matrix = new T[width,width];
         }
+
+        /// <summary>
+        /// Initializes the Matrix to square dimensions and fills it with the given items
+        /// </summary>
+        /// <param name="items">The number of items supplied *must* be a square number.</param>
+        public Matrix(params T[] items)
+        {
+            double width = Math.Sqrt(items.Length);
+
+            // If width isn't an integer, the pixel list isn't a square matrix
+            if(width != (int)width)
+            {
+                throw new ArgumentException("Matrix must be square - given number of pixels invalid!", "pixels");
+            }
+
+            _matrix = new T[(int)width, (int)width];
+            for(var i = 0; i < items.Length; i++)
+            {
+                _matrix[(int)((double)i/Width), i % Width] = items[i];
+            }
+        }
     }
 }
--- C#/Data Structures/1/1.7/1.7/DataStructures/Pixel.cs
+++ C#/Data Structures/1/1.7/1.7/DataStructures/Pixel.cs
@@ -12,5 +12,13 @@ namespace _1._7.DataStructures
         public byte Green { get; set; }
         public byte Blue { get; set; }
         public byte Alpha { get; set; }
+
+        public Pixel(byte r, byte g, byte b, byte a)
+        {
+            Red = r;
+            Green = g;
+            Blue = b;
+            Alpha = a;
+        }
     }
 }
