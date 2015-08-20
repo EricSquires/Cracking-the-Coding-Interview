1cd6fe53b6a8f65218a80fd5567691df63a5796f
Eric Squires <eric.h.squires@gmail.com>
2015-08-19 23:23:13 -0400

    Add tests, resolve bugs discovered in tests

--- C#/Data Structures/2/LinkedList/LinkedList/LinkedListNode.cs
+++ C#/Data Structures/2/LinkedList/LinkedList/LinkedListNode.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    public class LinkedListNode<T>
+    public class LinkedListNode<T> : IEquatable<LinkedListNode<T>>
     {
         public T Value { get; set; }
 
@@ -15,13 +15,14 @@ namespace LinkedList
 
         public LinkedListNode(IEnumerable<T> values)
         {
-            var currentNode = this;
+            LinkedListNode<T> currentNode = null;
 
             foreach(var val in values)
             {
-                if(currentNode == this)
+                if(currentNode == null)
                 {
                     Value = val;
+                    currentNode = this;
                 }
                 else
                 {
@@ -68,6 +69,85 @@ namespace LinkedList
             {
                 Next.Last = Last;
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is LinkedListNode<T> && Equals((LinkedListNode<T>)obj);
+        }
+
+        public bool Equals(LinkedListNode<T> other)
+        {
+            return Equals(other, Direction.Forward | Direction.Back);
+        }
+
+        private bool Equals(LinkedListNode<T> other, Direction direction)
+        {
+            if(!Value.Equals(other.Value))
+            {
+                return false;
+            }
+
+            if(direction.HasFlag(Direction.Back))
+            {
+                if(Last != null && other.Last != null)
+                {
+                    if(!Last.Equals(other.Last, Direction.Back))
+                    {
+                        return false;
+                    }
+                }
+                else if(Last != other.Last)
+                {
+                    return false;
+                }
+            }
+
+            if(direction.HasFlag(Direction.Forward))
+            {
+                if (Next != null && other.Next != null)
+                {
+                    if(!Next.Equals(other.Next, Direction.Forward))
+                    {
+                        return false;
+                    }
+                }
+                else if(Next != other.Next)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return GetString(Direction.Back | Direction.Forward);
+        }
+
+        private string GetString(Direction direction)
+        {
+            string ret = Value.ToString();
+
+            if(direction.HasFlag(Direction.Back) && Last != null)
+            {
+                ret = Last.GetString(Direction.Back) + ", " + ret;
+            }
+
+            if(direction.HasFlag(Direction.Forward))
+            {
+                ret += Next?.GetString(Direction.Forward);
+            }
+
+            return ret;
+        }
+
+        [Flags]
+        private enum Direction
+        {
+            Back = 1,
+            Forward = 2
         }
     }
 }
---
+++ C#/Data Structures/2/LinkedList/Tests/LinkedListTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
+
+namespace Tests
+{
+    [TestClass]
+    public class LinkedListTests
+    {
+        [TestMethod]
+        public void Equality()
+        {
+            var input = new LinkedListNode<int>(0, 1, 2, 3, 4, 5);
+            var expected = new LinkedListNode<int>(0, 1, 2, 3, 4, 5);
+
+            Assert.AreEqual(expected, input);
+        }
+    }
+}
