33e33d514b48e92520956901b106237a99ded7a1
Eric Squires <eric.h.squires@gmail.com>
2015-08-19 20:57:26 -0400

    Add initial in place solution and tests

---
+++ C#/Data Structures/1/1.7/1.7/Answers/RotatorInPlace.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._7.Answers
+{
+    public static class RotatorInPlace
+    {
+        /// <summary>
+        /// Rotates the given image (represented as a multidimensional array) clockwise.
+        /// </summary>
+        /// <param name="input">An image to be rotated</param>
+        public static void RotateCW(byte[,][] image)
+        {
+            var width = image.GetLength(0);
+
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    Swap(image, j, i, i, width - (j + 1));
+                }
+            }
+        }
+        public static void RotateCCW(byte[,][] image)
+        {
+            var width = image.GetLength(0);
+
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = width - 1; j >= 0; j--)
+                {
+                    Swap(image, i, width - (j + 1), j, i);
+                }
+            }
+        }
+
+        private static void Swap(byte[,][] image, int x1, int y1, int x2, int y2)
+        {
+            if(x1 == x2 && y1 == y2)
+            {
+                return;
+            }
+
+            for(var i = 0; i < image[x1, y1].Length; i++)
+            {
+                image[x1, y1][i] = (byte)(image[x1, y1][i] ^ image[x2, y2][i]);
+                image[x2, y2][i] = (byte)(image[x1, y1][i] ^ image[x2, y2][i]);
+                image[x1, y1][i] = (byte)(image[x1, y1][i] ^ image[x2, y2][i]);
+            }
+        }
+    }
+}
---
+++ C#/Data Structures/1/1.7/Tests/RotatorInPlaceTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1._7.Answers;
+using _1._7.DataStructures;
+
+namespace Tests
+{
+    [TestClass]
+    public class RotatorInPlaceTests
+    {
+        private bool ImagesEqual(byte[,][] a, byte[,][] b)
+        {
+            for(var i = 0; i < a.GetLength(0); i++)
+            {
+                for(var j = 0; j < a.GetLength(1); j++)
+                {
+                    for(var k = 0; k < a[i,j].Length; k++)
+                    {
+                        if(a[i,j][k] != b[i,j][k])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        [TestMethod]
+        public void RotateOne()
+        {
+            var image = new byte[,][]
+                {
+                    { new byte[] { 1, 2, 3, 4 } }
+                };
+            var expected = new byte[,][]
+                {
+                    { new byte[] { 1, 2, 3, 4 } }
+                };
+
+            RotatorInPlace.RotateCW(image);
+            Assert.IsTrue(ImagesEqual(expected, image));
+
+            image = new byte[,][]
+                {
+                    { new byte[] { 1, 2, 3, 4 } }
+                };
+            expected = new byte[,][]
+                {
+                { new byte[] { 1, 2, 3, 4 } }
+                };
+
+            RotatorInPlace.RotateCCW(image);
+            Assert.IsTrue(ImagesEqual(expected, image));
+        }
+
+        [TestMethod]
+        public void RotateTwo()
+        {
+            var image = new byte[,][]
+                {
+                    { new byte[] { 1, 1, 1, 1 }, new byte[] { 1, 1, 2, 2, } },
+                    { new byte[] { 2, 2, 1, 1 }, new byte[] { 2, 2, 2, 2, } }
+                };
+            var expected = new byte[,][]
+                {
+                    { new byte[] { 2, 2, 1, 1 }, new byte[] { 1, 1, 1, 1, } },
+                    { new byte[] { 2, 2, 2, 2 }, new byte[] { 1, 1, 2, 2, } }
+                };
+
+            RotatorInPlace.RotateCW(image);
+            Assert.IsTrue(ImagesEqual(expected, image));
+
+            image = new byte[,][]
+                {
+                    { new byte[] { 1, 1, 1, 1 }, new byte[] { 1, 1, 2, 2, } },
+                    { new byte[] { 2, 2, 1, 1 }, new byte[] { 2, 2, 2, 2, } }
+                };
+            expected = new byte[,][]
+                {
+                    { new byte[] { 1, 1, 2, 2 }, new byte[] { 2, 2, 2, 2, } },
+                    { new byte[] { 1, 1, 1, 1 }, new byte[] { 2, 2, 1, 1, } }
+                };
+
+            RotatorInPlace.RotateCW(image);
+            Assert.IsTrue(ImagesEqual(expected, image));
+        }
+
+        [TestMethod]
+        public void RotateThree()
+        {
+            var image = new byte[,][]
+                {
+                    { new byte[] { 1, 1, 1, 1 }, new byte[] { 1, 1, 2, 2, }, new byte[] { 1, 1, 3, 3 } },
+                    { new byte[] { 2, 2, 1, 1 }, new byte[] { 2, 2, 2, 2, }, new byte[] { 2, 2, 3, 3 } },
+                    { new byte[] { 3, 3, 1, 1 }, new byte[] { 3, 3, 2, 2, }, new byte[] { 3, 3, 3, 3 } }
+                };
+            var expected = new byte[,][]
+                {
+                    { new byte[] { 3, 3, 1, 1 }, new byte[] { 2, 2, 1, 1, }, new byte[] { 1, 1, 1, 1 } },
+                    { new byte[] { 3, 3, 2, 2 }, new byte[] { 2, 2, 2, 2, }, new byte[] { 1, 1, 2, 2 } },
+                    { new byte[] { 3, 3, 3, 3 }, new byte[] { 2, 2, 3, 3, }, new byte[] { 1, 1, 3, 3 } }
+                };
+
+            RotatorInPlace.RotateCW(image);
+            Assert.IsTrue(ImagesEqual(expected, image));
+
+            image = new byte[,][]
+                {
+                    { new byte[] { 1, 1, 1, 1 }, new byte[] { 1, 1, 2, 2, }, new byte[] { 1, 1, 3, 3 } },
+                    { new byte[] { 2, 2, 1, 1 }, new byte[] { 2, 2, 2, 2, }, new byte[] { 2, 2, 3, 3 } },
+                    { new byte[] { 3, 3, 1, 1 }, new byte[] { 3, 3, 2, 2, }, new byte[] { 3, 3, 3, 3 } }
+                };
+            expected = new byte[,][]
+                {
+                    { new byte[] { 1, 1, 3, 3 }, new byte[] { 2, 2, 3, 3, }, new byte[] { 3, 3, 3, 3 } },
+                    { new byte[] { 1, 1, 2, 2 }, new byte[] { 2, 2, 2, 2, }, new byte[] { 3, 3, 2, 2 } },
+                    { new byte[] { 1, 1, 1, 1 }, new byte[] { 2, 2, 1, 1, }, new byte[] { 3, 3, 1, 1 } }
+                };
+
+            RotatorInPlace.RotateCW(image);
+            Assert.IsTrue(ImagesEqual(expected, image));
+        }
+    }
+}
