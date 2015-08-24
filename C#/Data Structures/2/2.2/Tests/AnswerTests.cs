155ca4903d690ef6e4f8b30d65987a74f9f64064
Eric Squires <eric.h.squires@gmail.com>
2015-08-24 19:48:34 -0400

    Add unit tests

---
+++ C#/Data Structures/2/2.2/Tests/AnswerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
+using _2._2;
+
+namespace Tests
+{
+    [TestClass]
+    public class AnswerTests
+    {
+        [TestMethod]
+        public void LengthLessThanKTests()
+        {
+            var input = new LinkedListNode<int>(1, 2, 3, 4, 5, 6);
+
+            Assert.IsNull(input.GetKthToLast(6));
+        }
+
+        [TestMethod]
+        public void StandardTests()
+        {
+            var input = new LinkedListNode<int>(1, 2, 3, 4, 5, 6);
+
+            Assert.AreEqual(input.GetKthToLast(0).Value, 6);
+            Assert.AreEqual(input.GetKthToLast(1).Value, 5);
+            Assert.AreEqual(input.GetKthToLast(2).Value, 4);
+            Assert.AreEqual(input.GetKthToLast(3).Value, 3);
+            Assert.AreEqual(input.GetKthToLast(4).Value, 2);
+            Assert.AreEqual(input.GetKthToLast(5).Value, 1);
+        }
+    }
+}
