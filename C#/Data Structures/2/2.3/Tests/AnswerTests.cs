677ba8ef94b6fd98921471817037b8b49ae55ef9
Eric Squires <eric.h.squires@gmail.com>
2015-08-24 20:22:09 -0400

    Add tests

---
+++ C#/Data Structures/2/2.3/Tests/AnswerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
+using _2._3;
+
+namespace Tests
+{
+    [TestClass]
+    public class AnswerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void LastElementTests()
+        {
+            var input = new LinkedListNode<int>(1, 2, 3, 4, 5, 6);
+
+            for(; input.Next != null; input = input.Next);
+
+            input.Delete();
+        }
+
+        [TestMethod]
+        public void MidElementTests()
+        {
+            var input = new LinkedListNode<int>(1, 2, 3, 4, 5, 6);
+            Get(input, 1).Delete();
+            Assert.AreEqual(new LinkedListNode<int>(1, 3, 4, 5, 6), input);
+
+            input = new LinkedListNode<int>(1, 2, 3, 4, 5, 6);
+            Get(input, 2).Delete();
+            Assert.AreEqual(new LinkedListNode<int>(1, 2, 4, 5, 6), input);
+
+            input = new LinkedListNode<int>(1, 2, 3, 4, 5, 6);
+            Get(input, 3).Delete();
+            Assert.AreEqual(new LinkedListNode<int>(1, 2, 3, 5, 6), input);
+
+            input = new LinkedListNode<int>(1, 2, 3, 4, 5, 6);
+            Get(input, 4).Delete();
+            Assert.AreEqual(new LinkedListNode<int>(1, 2, 3, 4, 6), input);
+        }
+
+        private LinkedListNode<T> Get<T>(LinkedListNode<T> head, int index)
+        {
+            var ret = head;
+
+            for(var i = 0; i < index; i++, ret = ret.Next);
+
+            return ret;
+        }
+    }
+}
