3456e90efa4ca9927540fa0fa66fafc458929220
Eric Squires <eric.h.squires@gmail.com>
2015-08-25 21:29:26 -0400

    Add 2.4 solution

---
+++ C#/Data Structures/2/2.4/2.4/Answer.cs
@@ -0,0 +1,38 @@
+﻿using LinkedList;
+using System;
+
+namespace _2._4
+{
+    public static class Answer
+    {
+        public static LinkedListNode<T> Partition<T>(this LinkedListNode<T> head, T partitionValue) where T : IComparable
+        {
+            var high = new LinkedListNode<T>();
+            var low = new LinkedListNode<T>();
+            LinkedListNode<T> lowTail = null;
+
+            for(var current = head; current != null; current = current.Next)
+            {
+                if(current.Value.CompareTo(partitionValue) < 0)
+                {
+                    lowTail = low.Add(current.Value);
+                }
+                else
+                {
+                    high.Add(current.Value);
+                }
+            }
+
+            if(lowTail != null)
+            {
+                lowTail.Next = high;
+                high.Last = lowTail;
+                return low;
+            }
+            else
+            {
+                return high;
+            }
+        }
+    }
+}
