d2a958708b5f44a9391f465f0386bcadb6fd97a2
Eric Squires <eric.h.squires@gmail.com>
2015-08-26 20:02:20 -0400

    Add initial solution to 2.5

---
+++ C#/Data Structures/2/2.5/2.5/Answer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using LinkedList;
+
+namespace _2._5
+{
+    public static class Answer
+    {
+        private const int _DEFAULT_BASE = 10;
+
+        public static LinkedListNode<int> AddOnesFirst(LinkedListNode<int> a, LinkedListNode<int> b, int numBase = _DEFAULT_BASE)
+        {
+            var ret = new LinkedListNode<int>();
+            int remainder = 0;
+
+            while(a != null || b != null)
+            {
+                var val = remainder;
+
+                if(a != null)
+                {
+                    val += a.Value;
+                }
+
+                if(b != null)
+                {
+                    val += b.Value;
+                }
+
+                remainder = val % numBase;
+                val /= numBase; // Rely on integer division to get down to the actual value
+
+                ret.Add(val);
+
+                a = a?.Next;
+                b = b?.Next;
+            }
+
+            if(remainder > 0)
+            {
+                ret.Add(remainder);
+            }
+
+            return ret;
+        }
+    }
+}
