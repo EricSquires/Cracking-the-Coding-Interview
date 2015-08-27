8fe2595abcd470b0c2e254d936baf625e225b2ce
Eric Squires <eric.h.squires@gmail.com>
2015-08-26 21:22:02 -0400

    Add solution for 2.5 followup question - one's last implementation

--- C#/Data Structures/2/2.5/2.5/Answer.cs
+++ C#/Data Structures/2/2.5/2.5/Answer.cs
@@ -12,16 +12,16 @@ namespace _2._5
             var ret = new LinkedListNode<int>();
             int remainder = 0;
 
-            while(a != null || b != null)
+            while (a != null || b != null)
             {
                 var val = remainder;
 
-                if(a != null)
+                if (a != null)
                 {
                     val += a.Value;
                 }
 
-                if(b != null)
+                if (b != null)
                 {
                     val += b.Value;
                 }
@@ -35,12 +35,68 @@ namespace _2._5
                 b = b?.Next;
             }
 
-            if(remainder > 0)
+            if (remainder > 0)
             {
                 ret.Add(remainder);
             }
 
             return ret;
+        }
+        
+        public static LinkedListNode<int> AddOnesLast(LinkedListNode<int> a, LinkedListNode<int> b, int numBase = _DEFAULT_BASE)
+        {
+            var recurseRet = AddOnesLastRecursive(a, b, numBase);
+            LinkedListNode<int> ret;
+
+            if(recurseRet.Item1 != 0)
+            {
+                ret = new LinkedListNode<int>(recurseRet.Item1);
+                ret.Add(recurseRet.Item2);
+            }
+            else
+            {
+                ret = recurseRet.Item2;
+            }
+
+            return ret;
+        }
+
+        private static Tuple<int, LinkedListNode<int>> AddOnesLastRecursive(LinkedListNode<int> a, LinkedListNode<int> b, int numBase)
+        {
+            int remainder = 0;
+            int value = 0;
+
+            if(a == null && b == null)
+            {
+                return null;
+            }
+            else if(b == null || a.Length > b.Length)
+            {
+                value = a.Value;
+            }
+            else if(a == null || b.Length > a.Length)
+            {
+                value = b.Value;
+            }
+            else
+            {
+                value = a.Value + b.Value;
+
+                remainder = value / numBase;
+                value = value % numBase;
+            }
+            
+            var ret = new LinkedListNode<int>(value);
+
+            var nextRet = AddOnesLastRecursive(a?.Next, b?.Next, numBase);
+
+            if (nextRet != null)
+            {
+                ret.Value += nextRet.Item1;
+                ret.Add(nextRet.Item2);
+            }
+
+            return Tuple.Create(remainder, ret);
         }
     }
 }
