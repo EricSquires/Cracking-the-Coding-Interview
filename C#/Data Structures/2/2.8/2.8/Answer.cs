162c0d77557bf6c56e94025e1ae8744cf0979338
Eric Squires <eric.h.squires@gmail.com>
2015-08-28 19:24:00 -0400

    2.8: Add initial solution

---
+++ C#/Data Structures/2/2.8/2.8/Answer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LinkedList;
+
+namespace _2._8
+{
+    public static class Answer
+    {
+        public static LinkedListNode<T> GetLoopHead<T>(this LinkedListNode<T> head)
+        {
+            var slowNode = head;
+            var fastNode = head;
+
+            while(fastNode != null)
+            {
+                if(ReferenceEquals(slowNode, fastNode))
+                {
+                    for(var i = 0; i < head.Length - slowNode.Length; i++, head = head.Next);
+                    return head;
+                }
+
+                slowNode = slowNode.Next;
+                fastNode = fastNode.Next?.Next;
+            }
+
+            return null;
+        }
+    }
+}
