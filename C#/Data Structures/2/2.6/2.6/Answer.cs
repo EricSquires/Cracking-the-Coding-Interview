d0341438e31cff7295566cedb5b79d27f09cd9a2
Eric Squires <eric.h.squires@gmail.com>
2015-08-27 20:30:04 -0400

    Add initial 2.6 solution

---
+++ C#/Data Structures/2/2.6/2.6/Answer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using LinkedList;
+
+namespace _2._6
+{
+    public static class Answer
+    {
+        public static bool IsPalindrome<T>(this LinkedListNode<T> head) where T : IEquatable<T>
+        {
+            var stack = new System.Collections.Generic.Stack<T>();
+            var current = head;
+
+            int midFirst = head.Length / 2;
+            int midLast = midFirst + (head.Length % 2 == 0 ? 1 : 2);
+
+            for(var i = 0; i < head.Length; i++, current = current.Next)
+            {
+                if(i <= midFirst)
+                {
+                    stack.Push(current.Value);
+                }
+                else if(i >= midLast)
+                {
+                    var val = stack.Pop();
+
+                    if(!val.Equals(current.Value))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
