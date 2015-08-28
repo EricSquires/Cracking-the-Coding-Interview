06a3b2de020471170809fb1161509981a0d7a0b5
Eric Squires <eric.h.squires@gmail.com>
2015-08-27 20:50:11 -0400

    Add tests for 2.6

---
+++ C#/Data Structures/2/2.6/Tests/AnswerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2._6;
+using LinkedList;
+
+namespace Tests
+{
+    [TestClass]
+    public class AnswerTests
+    {
+        [TestMethod]
+        public void TrueTests()
+        {
+            var input = new LinkedListNode<int>(1, 2, 3, 2, 1);
+            
+            Assert.IsTrue(input.IsPalindrome());
+
+            input = new LinkedListNode<int>(1, 2, 3, 3, 2, 1);
+
+            Assert.IsTrue(input.IsPalindrome());
+        }
+
+        [TestMethod]
+        public void FalseTests()
+        {
+            var input = new LinkedListNode<int>(1, 2, 3, 4, 5);
+
+            Assert.IsFalse(input.IsPalindrome());
+
+            input = new LinkedListNode<int>(1, 2, 3, 4, 5, 6);
+
+            Assert.IsFalse(input.IsPalindrome());
+
+            input = new LinkedListNode<int>(1, 2, 3, 1, 1);
+
+            Assert.IsFalse(input.IsPalindrome());
+
+            input = new LinkedListNode<int>(1, 2, 3, 4, 2, 1);
+
+            Assert.IsFalse(input.IsPalindrome());
+        }
+    }
+}
