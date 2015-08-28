7cd94a23bde2fd08f9477bcc2b060c5d09e412a1
Eric Squires <eric.h.squires@gmail.com>
2015-08-28 19:02:30 -0400

    2.7: Add solution without using HashSet and accompanying tests

--- C#/Data Structures/2/2.7/2.7/Answer.cs
+++ C#/Data Structures/2/2.7/2.7/Answer.cs
@@ -9,20 +9,51 @@ namespace _2._7
         {
             var set = new System.Collections.Generic.HashSet<LinkedListNode<T>>(new ReferenceComparer<LinkedListNode<T>>());
 
-            for(var current = a; current.Next != null; current = current.Next)
+            for (var current = a; current.Next != null; current = current.Next)
             {
                 set.Add(current);
             }
 
-            for(var current = b; current.Next != null; current = current.Next)
+            for (var current = b; current.Next != null; current = current.Next)
             {
-                if(!set.Add(current))
+                if (!set.Add(current))
                 {
                     return current;
                 }
             }
 
             return null;
+        }
+        public static LinkedListNode<T> GetIntersectionNoSet<T>(LinkedListNode<T> a, LinkedListNode<T> b)
+        {
+            if(a.Length > b.Length)
+            {
+                a = TraverseTo(a, a.Length - b.Length);
+            }
+            else if (b.Length > a.Length)
+            {
+                b = TraverseTo(b, b.Length - a.Length);
+            }
+
+            do
+            {
+                if(ReferenceEquals(a, b))
+                {
+                    return a;
+                }
+
+                a = a.Next;
+                b = b.Next;
+            }
+            while(a != null && b != null);
+
+            return null;
+        }
+
+        private static LinkedListNode<T> TraverseTo<T>(LinkedListNode<T> head, int index)
+        {
+            for(var i = 0; i < index; i++, head = head.Next);
+            return head;
         }
 
         private class ReferenceComparer<T> : System.Collections.Generic.IEqualityComparer<T>
--- C#/Data Structures/2/2.7/Tests/AnswerTests.cs
+++ C#/Data Structures/2/2.7/Tests/AnswerTests.cs
@@ -15,16 +15,16 @@ namespace Tests
         {
             var inputOne = new LinkedListNode<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-            for(var currentInt = inputOne; currentInt.Next != null; currentInt = currentInt.Next)
+            for (var currentInt = inputOne; currentInt.Next != null; currentInt = currentInt.Next)
             {
-                for(var i = 0; i < 10; i++)
+                for (var i = 0; i < 10; i++)
                 {
                     var inputTwo = new LinkedListNode<int>(11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
 
                     var currentRepl = inputTwo;
-                    for (var j = 0; j < i; j++, currentRepl = currentRepl.Next);
+                    for (var j = 0; j < i; j++, currentRepl = currentRepl.Next) ;
 
-                    if(currentRepl.Last == null)
+                    if (currentRepl.Last == null)
                     {
                         inputTwo = currentInt;
                     }
@@ -49,6 +49,47 @@ namespace Tests
             inputTwo = new LinkedListNode<int>(11);
 
             Assert.IsNull(Answer.GetIntersection(inputOne, inputTwo));
+        }
+
+        [TestMethod]
+        public void IntersectionTestsNoSet()
+        {
+            var inputOne = new LinkedListNode<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+            for (var currentInt = inputOne; currentInt.Next != null; currentInt = currentInt.Next)
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    var inputTwo = new LinkedListNode<int>(11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+
+                    var currentRepl = inputTwo;
+                    for (var j = 0; j < i; j++, currentRepl = currentRepl.Next) ;
+
+                    if (currentRepl.Last == null)
+                    {
+                        inputTwo = currentInt;
+                    }
+                    else
+                    {
+                        currentRepl.Last.Next = currentInt;
+                    }
+
+                    Assert.AreEqual(currentInt, Answer.GetIntersectionNoSet(inputOne, inputTwo));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void NonIntersectionTestsNoSet()
+        {
+            var inputOne = new LinkedListNode<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            var inputTwo = new LinkedListNode<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+            Assert.IsNull(Answer.GetIntersectionNoSet(inputOne, inputTwo));
+
+            inputTwo = new LinkedListNode<int>(11);
+
+            Assert.IsNull(Answer.GetIntersectionNoSet(inputOne, inputTwo));
         }
     }
 }
