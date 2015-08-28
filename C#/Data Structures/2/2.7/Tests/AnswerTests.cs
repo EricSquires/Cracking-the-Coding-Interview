cad49edfc4fb41ac01ba0c73a2e1f32373f61cdb
Eric Squires <eric.h.squires@gmail.com>
2015-08-28 18:51:12 -0400

    2.7: Add tests

---
+++ C#/Data Structures/2/2.7/Tests/AnswerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2._7;
+using LinkedList;
+
+namespace Tests
+{
+    [TestClass]
+    public class AnswerTests
+    {
+        private static readonly Random _RAND = new Random();
+
+        [TestMethod]
+        public void IntersectionTests()
+        {
+            var inputOne = new LinkedListNode<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+            for(var currentInt = inputOne; currentInt.Next != null; currentInt = currentInt.Next)
+            {
+                for(var i = 0; i < 10; i++)
+                {
+                    var inputTwo = new LinkedListNode<int>(11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+
+                    var currentRepl = inputTwo;
+                    for (var j = 0; j < i; j++, currentRepl = currentRepl.Next);
+
+                    if(currentRepl.Last == null)
+                    {
+                        inputTwo = currentInt;
+                    }
+                    else
+                    {
+                        currentRepl.Last.Next = currentInt;
+                    }
+
+                    Assert.AreEqual(currentInt, Answer.GetIntersection(inputOne, inputTwo));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void NonIntersectionTests()
+        {
+            var inputOne = new LinkedListNode<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            var inputTwo = new LinkedListNode<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+            Assert.IsNull(Answer.GetIntersection(inputOne, inputTwo));
+
+            inputTwo = new LinkedListNode<int>(11);
+
+            Assert.IsNull(Answer.GetIntersection(inputOne, inputTwo));
+        }
+    }
+}
