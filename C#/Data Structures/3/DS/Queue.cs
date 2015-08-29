563e019fb6919a105dd594ca48cf0bbaabb7a698
Eric Squires <eric.h.squires@gmail.com>
2015-08-29 19:14:18 -0400

    3: Initial commit

---
+++ C#/Data Structures/3/DS/Queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS
+{
+    public class Queue<T>
+    {
+        private T[] _values;
+        private int _currentIndex = 0;
+        
+        public int Length { get; private set; } = 0;
+
+        public Queue(int size)
+        {
+            _values = new T[size];
+        }
+
+        /// <summary>
+        /// Adds <paramref name="value"/> to the queue. Returns this queue for convenient chaining of Enqueue calls.
+        /// </summary>
+        public Queue<T> Enqueue(T value)
+        {
+            if(Length == _values.Length)
+            {
+                throw new InvalidOperationException("Queue is full");
+            }
+
+            _values[Length] = value;
+            Length++;
+            return this;
+        }
+
+        /// <summary>
+        /// Returns the next element in the queue and removes it from the queue.
+        /// </summary>
+        public T Dequeue()
+        {
+            if(Length == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            var ret = Peek();
+
+            Length--;
+            _currentIndex = (_currentIndex + 1) % _values.Length;
+            return ret;
+        }
+
+        /// <summary>
+        /// Returns the next element in the queue.
+        /// </summary>
+        public T Peek()
+        {
+            return _values[_currentIndex];
+        }
+    }
+}
---
+++ C#/Data Structures/3/DS/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS
+{
+    public class Stack<T>
+    {
+        private T[] _values;
+        int _currentIndex = -1;
+
+        public int Length
+        {
+            get { return _currentIndex + 1; }
+        }
+
+        public Stack(int size)
+        {
+            _values = new T[size];
+        }
+
+        /// <summary>
+        /// Adds <paramref name="value"/> to the stack. Returns this stack for convenient chaining of Push calls.
+        /// </summary>
+        public Stack<T> Push(T value)
+        {
+            if(_currentIndex == _values.Length - 1)
+            {
+                throw new InvalidOperationException("Stack is full");
+            }
+
+            _currentIndex++;
+            _values[_currentIndex] = value;
+            return this;
+        }
+
+        /// <summary>
+        /// Returns the top element in the stack and removes it from the stack.
+        /// </summary>
+        public T Pop()
+        {
+            if(_currentIndex < 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            var ret = Peek();
+            _currentIndex--;
+            return ret;
+        }
+
+        /// <summary>
+        /// Returns the top element in the stack.
+        /// </summary>
+        public T Peek()
+        {
+            return _values[_currentIndex];
+        }
+    }
+}
