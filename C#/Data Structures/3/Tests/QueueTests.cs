89e22b81c2e212e7154f50ca5d65f0279547d2c8
Eric Squires <eric.h.squires@gmail.com>
2015-08-29 19:46:47 -0400

    3: Add unit tests for the data structures

---
+++ C#/Data Structures/3/Tests/QueueTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DS;
+
+namespace Tests
+{
+    [TestClass]
+    public class QueueTests
+    {
+        [TestMethod]
+        public void InitializeTests()
+        {
+            var q = new Queue<int>(10);
+
+            Assert.IsTrue(q.IsEmpty);
+        }
+
+        [TestMethod]
+        public void EnqueueTests()
+        {
+            var q = new Queue<int>(10);
+
+            for(var i = 0; i < q.MaxLength; i++)
+            {
+                q.Enqueue(i);
+
+                Assert.AreEqual(0, q.Peek());
+                Assert.AreEqual(i + 1, q.Length);
+            }
+
+            Assert.AreEqual(q.MaxLength, q.Length);
+        }
+
+        [TestMethod]
+        public void DequeueTests()
+        {
+            var q = new Queue<int>(10);
+
+            for(var i = 0; i < q.MaxLength; i++)
+            {
+                q.Enqueue(i);
+            }
+
+            Assert.AreEqual(q.Length, q.MaxLength);
+
+            for(var i = 0; i < q.MaxLength; i++)
+            {
+                Assert.AreEqual(i, q.Peek());
+                Assert.AreEqual(i, q.Dequeue());
+                Assert.AreEqual(q.MaxLength - (i + 1), q.Length);
+            }
+
+            Assert.IsTrue(q.IsEmpty);
+        }
+    }
+}
---
+++ C#/Data Structures/3/Tests/StackTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DS;
+
+namespace Tests
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        public void InitializeTests()
+        {
+            var s = new Stack<int>(10);
+
+            Assert.IsTrue(s.IsEmpty);
+            Assert.AreEqual(0, s.Length);
+            Assert.AreEqual(10, s.MaxLength);
+        }
+
+        [TestMethod]
+        public void PushTests()
+        {
+            var s = new Stack<int>(10);
+
+            for (var i = 0; i < s.MaxLength; i++)
+            {
+                s.Push(i);
+
+                Assert.AreEqual(i, s.Peek());
+                Assert.AreEqual(i + 1, s.Length);
+            }
+
+            Assert.AreEqual(s.MaxLength, s.Length);
+        }
+
+        [TestMethod]
+        public void PopTests()
+        {
+            var s = new Stack<int>(10);
+
+            for (var i = 0; i < s.MaxLength; i++)
+            {
+                s.Push(i);
+            }
+
+            Assert.AreEqual(s.Length, s.MaxLength);
+
+            for (var i = s.MaxLength - 1; i >= 0; i--)
+            {
+                Assert.AreEqual(i, s.Peek());
+                Assert.AreEqual(i, s.Pop());
+                Assert.AreEqual(i, s.Length);
+            }
+
+            Assert.IsTrue(s.IsEmpty);
+        }
+    }
+}
