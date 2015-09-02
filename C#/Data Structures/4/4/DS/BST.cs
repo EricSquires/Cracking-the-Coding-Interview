d17554893345c5e70228aacaac97d1e2ab8e5730
Eric Squires <eric.h.squires@gmail.com>
2015-09-02 00:02:06 -0400

    4.2: Add initial BST implementation

---
+++ C#/Data Structures/4/4/DS/BST.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS
+{
+    public class BST<T> where T : IComparable<T>
+    {
+        public BST<T> Left { get; private set; }
+        public BST<T> Right { get; private set; }
+
+        public T Value { get; }
+
+        public BST(T value)
+        {
+            Value = value;
+        }
+
+        public void Insert(T value)
+        {
+            if(value.CompareTo(Value) < 0)
+            {
+                if(Left == null)
+                {
+                    Left = new BST<T>(value);
+                }
+                else
+                {
+                    Left.Insert(value);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = new BST<T>(value);
+                }
+                else
+                {
+                    Right.Insert(value);
+                }
+            }
+        }
+    }
+}
