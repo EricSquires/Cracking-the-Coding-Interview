f9621968062c9eb7171ca654d5599f351870f991
Eric Squires <eric.h.squires@gmail.com>
2015-09-01 23:48:55 -0400

    4.1: Add intiial solution via DFS

---
+++ C#/Data Structures/4/4/4/RouteBetween.cs
@@ -0,0 +1,41 @@
+﻿using DS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    public class RouteBetween<T> : DirectedGraphNode<T>
+    {
+        public RouteBetween(T value) : base(value) { }
+
+        public bool HasRoute(RouteBetween<T> dest, HashSet<RouteBetween<T>> touched = null)
+        {
+            if(_neighbors.ContainsKey(dest))
+            {
+                return true;
+            }
+
+            if(touched == null)
+            {
+                touched = new HashSet<RouteBetween<T>>();
+            }
+
+            touched.Add(this);
+
+            var searchNodes = _neighbors.Keys.Where(k => !touched.Contains(k)).Cast<RouteBetween<T>>();
+
+            foreach (var n in searchNodes)
+            {
+                if(n.HasRoute(dest))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
