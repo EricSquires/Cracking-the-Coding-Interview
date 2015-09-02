15cd58b9ae9f78bc6b7df342331f5c20e930ad64
Eric Squires <eric.h.squires@gmail.com>
2015-09-01 23:33:28 -0400

    4: Initial commit with basic GraphNode implementation

---
+++ C#/Data Structures/4/4/DS/GraphNode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS
+{
+    public class GraphNode<T>
+    {
+        public T Value { get; set; }
+
+        private readonly Dictionary<GraphNode<T>, double> _neighbors = new Dictionary<GraphNode<T>, double>();
+        public IReadOnlyCollection<GraphNode<T>> Neighbors => _neighbors.Keys.ToList().AsReadOnly();
+        
+        public GraphNode(T value, params GraphNode<T>[] neighbors)
+        {
+            Value = value;
+
+            foreach(var n in neighbors)
+            {
+                ConnectTo(n);
+            }
+        }
+
+        /// <summary>
+        /// Connects this node to the given one and returns this node for easy chaining of statements.
+        /// </summary>
+        public virtual GraphNode<T> ConnectTo(GraphNode<T> neighbor)
+        {
+            return ConnectTo(neighbor, 1);
+        }
+
+        /// <summary>
+        /// Connects this node to the given one and returns this node for easy chaining of statements.
+        /// </summary>
+        public virtual GraphNode<T> ConnectTo(GraphNode<T> neighbor, double weight)
+        {
+            _neighbors[neighbor] = weight;
+            neighbor._neighbors[this] = weight;
+
+            return this;
+        }
+    }
+}
