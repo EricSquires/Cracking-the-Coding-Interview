68df3f7f87af28356be29606f53fc1e086d1bb5b
Eric Squires <eric.h.squires@gmail.com>
2015-09-04 23:21:08 -0400

    4.5: Add intiial solution

---
+++ C#/Data Structures/4/4/4/ValidateBST.cs
@@ -0,0 +1,31 @@
+﻿using DS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    public static class ValidateBST
+    {
+        public static bool IsBST<T>(this BinaryTree<T> root) where T : IComparable<T>
+        {
+            if(root == null) return true;
+
+            bool isSorted = true;
+
+            if(root.Left != null)
+            {
+                isSorted = isSorted && root.Left.Value.CompareTo(root.Value) <= 0;
+            }
+
+            if(root.Right != null)
+            {
+                isSorted = isSorted && root.Right.Value.CompareTo(root.Value) >= 0;
+            }
+            
+            return isSorted && root.Left.IsBST() && root.Right.IsBST();
+        }
+    }
+}
