91047602343fcf88a87b1f5043101d6bb2360707
Eric Squires <eric.h.squires@gmail.com>
2015-09-04 23:38:02 -0400

    4.4: Add tests

---
+++ C#/Data Structures/4/4/Tests/CheckBalanceTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DS;
+using _4;
+
+namespace Tests
+{
+    [TestClass]
+    public class CheckBalanceTests
+    {
+        [TestMethod]
+        public void BalancedTests()
+        {
+            var input = new BST<int>(5);
+
+            input.Insert(3);
+            input.Insert(1);
+            input.Insert(4);
+            input.Insert(2);
+
+            input.Insert(7);
+            input.Insert(6);
+            input.Insert(8);
+            input.Insert(9);
+
+            Assert.IsTrue(input.IsBalanced());
+
+            input = new BST<int>(5);
+
+            input.Insert(3);
+            input.Insert(1);
+            input.Insert(4);
+            input.Insert(2);
+
+            input.Insert(7);
+            input.Insert(6);
+            input.Insert(8);
+
+            Assert.IsTrue(input.IsBalanced());
+        }
+
+        [TestMethod]
+        public void NotBalancedTests()
+        {
+            var input = new BST<int>(10);
+
+            input.Insert(3);
+            input.Insert(1);
+            input.Insert(4);
+            input.Insert(2);
+
+            input.Insert(7);
+            input.Insert(6);
+            input.Insert(8);
+            input.Insert(9);
+
+            Assert.IsFalse(input.IsBalanced());
+
+            input = new BST<int>(0);
+
+            input.Insert(3);
+            input.Insert(1);
+            input.Insert(4);
+            input.Insert(2);
+
+            input.Insert(7);
+            input.Insert(6);
+            input.Insert(8);
+            input.Insert(9);
+
+            Assert.IsFalse(input.IsBalanced());
+
+            input = new BST<int>(2);
+
+            input.Insert(3);
+            input.Insert(1);
+            input.Insert(4);
+            input.Insert(2);
+
+            input.Insert(7);
+            input.Insert(6);
+            input.Insert(8);
+            input.Insert(9);
+
+            Assert.IsFalse(input.IsBalanced());
+
+            input = new BST<int>(7);
+
+            input.Insert(3);
+            input.Insert(1);
+            input.Insert(4);
+            input.Insert(2);
+
+            input.Insert(7);
+            input.Insert(6);
+
+            Assert.IsFalse(input.IsBalanced());
+        }
+    }
+}
