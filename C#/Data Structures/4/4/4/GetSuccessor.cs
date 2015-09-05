ffcd250e1e94413c4311cc2df7ef41c2f2f3c015
Eric Squires <eric.h.squires@gmail.com>
2015-09-04 23:29:13 -0400

    4.6: Add initial solution

---
+++ C#/Data Structures/4/4/4/GetSuccessor.cs
@@ -0,0 +1,19 @@
+﻿using DS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    public static class GetNext
+    {
+        public static BST<T> Next<T>(this BST<T> node) where T : IComparable<T>
+        {
+            if(node == null) return null;
+
+            return node.Right.Next() ?? node.Parent.Next() ?? node;
+        }
+    }
+}
