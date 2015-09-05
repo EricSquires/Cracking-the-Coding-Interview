428cf8aa25f171dc8989024f0afe9a33a95b6342
Eric Squires <eric.h.squires@gmail.com>
2015-09-04 23:09:24 -0400

    4.4: Add initial solution

---
+++ C#/Data Structures/4/4/4/CheckBalance.cs
@@ -0,0 +1,27 @@
+﻿using DS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    public static class CheckBalance
+    {
+        public static bool IsBalanced<T>(this BinaryTree<T> root)
+        {
+            return Math.Abs(CountDepth(root.Left) - CountDepth(root.Right)) <= 1;
+        }
+
+        private static int CountDepth<T>(BinaryTree<T> root)
+        {
+            if(root == null) return 0;
+
+            int leftDepth = CountDepth(root.Left);
+            int rightDepth = CountDepth(root.Right);
+
+            return Math.Max(leftDepth, rightDepth) + 1;
+        }
+    }
+}
