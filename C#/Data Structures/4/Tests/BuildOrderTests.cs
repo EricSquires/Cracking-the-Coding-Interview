d2d7a4df3dbbee1bd49a98ce59eec3dc05657e2f
Eric Squires <eric.h.squires@gmail.com>
2015-09-06 15:26:05 -0400

    4.7: Add first test

---
+++ C#/Data Structures/4/Tests/BuildOrderTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _4;
+using System.Linq;
+
+namespace Tests
+{
+    /// <summary>
+    /// Summary description for BuildOrderTests
+    /// </summary>
+    [TestClass]
+    public class BuildOrderTests
+    {
+        private List<int> _input = new List<int>(Enumerable.Range(0, 100));
+
+        [TestMethod]
+        public void NoDependenciesTests()
+        {
+            var bo = new BuildOrder<int>(_input, new List<Tuple<int, int>>());
+            Assert.IsTrue(bo.CanBuild());
+        }
+    }
+}
