9856fa086d7324458265a5bc56397a9947b37fb9
Eric Squires <eric.h.squires@gmail.com>
2016-01-28 17:49:47 -0700

    Refactored tests, added new ones and refactored solution for the BST sequencing problem in order to work towards a more complete solution

--- C#/Data Structures/4/4/BST_Sequences.cs
+++ C#/Data Structures/4/4/BST_Sequences.cs
@@ -9,25 +9,123 @@ namespace _4
 {
     public static class BST_Sequences
     {
-        public static LinkedList<LinkedList<T>> GetSequences<T>(BST<T> root) where T : IComparable<T>
+        public static IEnumerable<IEnumerable<T>> GetSequences<T>(BST<T> root) where T : IComparable<T>
         {
-            var ret = new LinkedList<LinkedList<T>>();
+            var ret = new List<IEnumerable<T>>();
 
-            var depthLists = DepthsLists.GetDepthLists(root);
-
-            foreach(var dList in depthLists)
+            if(root == null)
             {
-                if(ret.Count == 0)
+                return ret;
+            }
+
+            var leftPerms = GetSequences(root.Left);
+            var rightPerms = GetSequences(root.Right);
+
+            foreach(var lp in leftPerms)
+            {
+                foreach(var rp in rightPerms)
                 {
-                    ret.AddLast(new LinkedList<T>(dList.Select(n => n.Value)));
+                    var perms = GetPermutations(lp.ToList(), rp.ToList());
+
+                    foreach(var perm in perms)
+                    {
+                        var lst = new List<T>();
+                        lst.Add(root.Value);
+                        lst.AddRange(perm);
+
+                        ret.Add(lst);
+                    }
                 }
-                else
+            }
+
+            if(ret.Count == 0)
+            {
+                ret.Add(new[] { root.Value });
+            }
+
+            //var depthLists = DepthsLists.GetDepthLists(root);
+
+            //foreach(var dList in depthLists)
+            //{
+            //    if(ret.Count == 0)
+            //    {
+            //        ret.AddLast(new LinkedList<T>(dList.Select(n => n.Value)));
+            //    }
+            //    else
+            //    {
+            //        ret = BuildNewList(dList, ret);
+            //    }
+            //}
+
+            return ret;
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(IList<T> a, IList<T> b)
+        {
+            var ret = new List<IEnumerable<T>>();
+
+            if((a == null || a.Count == 0) && (b == null || b.Count == 0))
+            {
+                return ret;
+            }
+
+            ret.Add(new LinkedList<T>(a).AddRange(b));
+
+            for(var i = a.Count - 1; i >= 0; i--)
+            {
+                for(var j = 1; j <= b.Count; j++)
                 {
-                    ret = BuildNewList(dList, ret);
+                    foreach(var perm in GetPermutations(a.Skip(i).ToList(), b.Skip(j).ToList()))
+                    {
+                        var lst = a.Take(i).ToList();
+                        lst.AddRange(b.Take(j));
+                        lst.AddRange(perm);
+
+                        if(!ListsAreEqual(ret.Last().ToList(), lst))
+                        {
+                            ret.Add(lst);
+                        }
+                    }
                 }
             }
 
             return ret;
+        }
+
+        private static bool ListsAreEqual<T>(IList<T> a, IList<T> b)
+        {
+            if(a.Count != b.Count)
+            {
+                return false;
+            }
+
+            for(var i = 0; i < a.Count; i++)
+            {
+                if(!a[i].Equals(b[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static Tuple<IEnumerable<IEnumerable<T>>, IEnumerable<IEnumerable<T>>> GetChildSequences<T>(BST<T> root) where T : IComparable<T>
+        {
+            IEnumerable<IEnumerable<T>> leftSeq = null;
+            IEnumerable<IEnumerable<T>> rightSeq = null;
+
+            if(root.Left != null)
+            {
+                leftSeq = GetSequences(root.Left);
+            }
+
+            if(root.Right != null)
+            {
+                rightSeq = GetSequences(root.Right);
+            }
+
+            return Tuple.Create(leftSeq, rightSeq);
         }
 
         private static LinkedList<LinkedList<T>> BuildNewList<T>(LinkedList<BinaryTree<T>> depthLst, LinkedList<LinkedList<T>> currentLst)
--- C#/Data Structures/4/Tests/BST_SequencesTests.cs
+++ C#/Data Structures/4/Tests/BST_SequencesTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using _4;
 using DS;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Tests
 {
@@ -16,14 +17,12 @@ namespace Tests
             bst.Insert(1);
             bst.Insert(3);
 
-            var actual = BST_Sequences.GetSequences(bst);
+            var actual = BST_Sequences.GetSequences(bst).ToArray();
 
-            Assert.AreEqual(2, actual.Count);
+            Assert.AreEqual(2, actual.Length);
 
-            Assert.IsTrue(VerifyOutput(new[] { 2, 1, 3 }, actual.First.Value));
-            actual.RemoveFirst();
-
-            Assert.IsTrue(VerifyOutput(new[] { 2, 3, 1 }, actual.First.Value));
+            Assert.IsTrue(VerifyOutput(new[] { 2, 1, 3 }, actual));
+            Assert.IsTrue(VerifyOutput(new[] { 2, 3, 1 }, actual));
         }
 
         [TestMethod]
@@ -40,11 +39,11 @@ namespace Tests
             bst.Insert(8);
             bst.Insert(9);
 
-            var actual = BST_Sequences.GetSequences(bst);
+            var actual = BST_Sequences.GetSequences(bst).ToArray();
 
-            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(1, actual.Length);
 
-            Assert.IsTrue(VerifyOutput(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, actual.First.Value));
+            Assert.IsTrue(VerifyOutput(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, actual));
 
             bst = new BST<int>(9);
             bst.Insert(8);
@@ -57,11 +56,11 @@ namespace Tests
             bst.Insert(1);
             bst.Insert(0);
 
-            actual = BST_Sequences.GetSequences(bst);
+            actual = BST_Sequences.GetSequences(bst).ToArray();
 
-            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(1, actual.Length);
 
-            Assert.IsTrue(VerifyOutput(new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, actual.First.Value));
+            Assert.IsTrue(VerifyOutput(new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, actual));
         }
 
         [TestMethod]
@@ -73,35 +72,68 @@ namespace Tests
             bst.Insert(7);
             bst.Insert(5);
 
-            var actual = BST_Sequences.GetSequences(bst);
+            var actual = BST_Sequences.GetSequences(bst).ToArray();
 
-            Assert.AreEqual(6, actual.Count);
+            Assert.AreEqual(6, actual.Length, GetTestMessage(actual));
 
-            Assert.IsTrue(VerifyOutput(new[] { 3, 2, 7, 2, 5 }, actual.First.Value));
-            actual.RemoveFirst();
-
-            Assert.IsTrue(VerifyOutput(new[] { 3, 2, 7, 5, 2 }, actual.First.Value));
-            actual.RemoveFirst();
-
-            Assert.IsTrue(VerifyOutput(new[] { 3, 7, 2, 5, 2 }, actual.First.Value));
-            actual.RemoveFirst();
-
-            Assert.IsTrue(VerifyOutput(new[] { 3, 7, 2, 2, 5 }, actual.First.Value));
-            actual.RemoveFirst();
-
-            Assert.IsTrue(VerifyOutput(new[] { 3, 2, 2, 7, 5 }, actual.First.Value));
-            actual.RemoveFirst();
-
-            Assert.IsTrue(VerifyOutput(new[] { 3, 7, 5, 2, 2 }, actual.First.Value));
-            actual.RemoveFirst();
+            Assert.IsTrue(VerifyOutput(new[] { 3, 2, 7, 2, 5 }, actual));
+            Assert.IsTrue(VerifyOutput(new[] { 3, 2, 7, 5, 2 }, actual));
+            Assert.IsTrue(VerifyOutput(new[] { 3, 7, 2, 5, 2 }, actual));
+            Assert.IsTrue(VerifyOutput(new[] { 3, 7, 2, 2, 5 }, actual));
+            Assert.IsTrue(VerifyOutput(new[] { 3, 2, 2, 7, 5 }, actual));
+            Assert.IsTrue(VerifyOutput(new[] { 3, 7, 5, 2, 2 }, actual));
         }
 
-        private bool VerifyOutput(IList<int> expected, IEnumerable<int> actual)
+        [TestMethod]
+        public void GetPermutationsSimpleTests()
         {
-            var i = 0;
+            var actual = BST_Sequences.GetPermutations(new[] { 1 }, new[] { 4 }).ToArray();
+
+            Assert.AreEqual(2, actual.Length, GetTestMessage(actual));
+
+            Assert.IsTrue(VerifyOutput(new[] { 1, 4 }, actual), GetTestMessage(actual));
+            Assert.IsTrue(VerifyOutput(new[] { 4, 1 }, actual), GetTestMessage(actual));
+        }
+
+        [TestMethod]
+        public void GetPermutationsTests()
+        {
+            var actual = BST_Sequences.GetPermutations(new[] { 1, 2 }, new[] { 4, 5 }).ToArray();
+
+            Assert.AreEqual(6, actual.Length, GetTestMessage(actual));
+
+            Assert.IsTrue(VerifyOutput(new[] { 1, 2, 4, 5 }, actual), GetTestMessage(actual));
+            Assert.IsTrue(VerifyOutput(new[] { 1, 4, 2, 5 }, actual), GetTestMessage(actual));
+            Assert.IsTrue(VerifyOutput(new[] { 1, 4, 5, 2 }, actual), GetTestMessage(actual));
+            Assert.IsTrue(VerifyOutput(new[] { 4, 1, 2, 5 }, actual), GetTestMessage(actual));
+            Assert.IsTrue(VerifyOutput(new[] { 4, 1, 5, 2 }, actual), GetTestMessage(actual));
+            Assert.IsTrue(VerifyOutput(new[] { 4, 5, 1, 2 }, actual), GetTestMessage(actual));
+        }
+
+        private string GetTestMessage<T>(IEnumerable<IEnumerable<T>> actual)
+        {
+            return Environment.NewLine + string.Join(Environment.NewLine, actual.Select(s => string.Join(", ", s)));
+        }
+
+        private bool VerifyOutput(IList<int> expected, IEnumerable<IEnumerable<int>> actual)
+        {
             foreach(var a in actual)
             {
-                if(expected[i] != a)
+                if(VerifyOutputItem(expected, a))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool VerifyOutputItem(IList<int> expected, IEnumerable<int> actual)
+        {
+            var i = 0;
+            foreach (var a in actual)
+            {
+                if (expected[i] != a)
                 {
                     return false;
                 }
