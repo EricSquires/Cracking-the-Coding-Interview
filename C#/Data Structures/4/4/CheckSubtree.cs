163add09c9ea35d103bb1547dcc94826c8c04ff2
Eric Squires <eric.h.squires@gmail.com>
2016-02-03 13:27:29 -0700

    Initial (naive) implementation of 4.10 solution along with first test case

---
+++ C#/Data Structures/4/4/CheckSubtree.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    public class CheckSubtree<T>
+    {
+        private int _numChildren = 0;
+
+        private CheckSubtree<T> _parent;
+
+        private CheckSubtree<T> _left;
+        public CheckSubtree<T> Left
+        {
+            get { return _left; }
+            set
+            {
+                _left = value;
+
+                if (Left != null)
+                {
+                    Left._parent = this;
+                    _numChildren += Left._numChildren + 1;
+                }
+            }
+        }
+
+        private CheckSubtree<T> _right;
+        public CheckSubtree<T> Right
+        {
+            get { return _right; }
+            set
+            {
+                _right = value;
+
+                if (Right != null)
+                {
+                    Right._parent = this;
+                    _numChildren += Right._numChildren + 1;
+                }
+            }
+        }
+
+        public T Value { get; set; }
+
+        public CheckSubtree(T value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Checks if the given tree is a subtree of this one
+        /// </summary>
+        /// <returns>true if <paramref name="other"/> is a subtree of this node, false otherwise</returns>
+        public bool IsSubtree(CheckSubtree<T> other)
+        {
+            return other._numChildren == _numChildren && IsSubtreeCheckValues(other);
+        }
+
+        private bool IsSubtreeCheckValues(CheckSubtree<T> other)
+        {
+            return other.Value.Equals(Value)
+                && (Left == null || Left.IsSubtreeCheckValues(other.Left))
+                && (Right == null || Right.IsSubtreeCheckValues(other.Right));
+        }
+
+        public CheckSubtree<T> Clone()
+        {
+            var clone = new CheckSubtree<T>(Value);
+
+            clone.Left = Left?.Clone();
+            clone.Right = Right?.Clone();
+            
+            return clone;
+        }
+    }
+}
---
+++ C#/Data Structures/4/Tests/CheckSubtreeTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class CheckSubtreeTests
+    {
+        [TestMethod()]
+        public void IsSubtreeTest()
+        {
+            var a = new CheckSubtree<int>(1);
+            var b = a.Clone();
+
+            Assert.IsTrue(a.IsSubtree(b));
+        }
+    }
+}
